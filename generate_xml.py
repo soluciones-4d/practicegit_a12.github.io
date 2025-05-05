51cfedc993a0de4a2e7810b54229072332065391
soluciones-4d <provasectic@gmail.com>
2025-05-05 19:37:29 +0200

    Update generate_xml.py

--- generate_xml.py
+++ generate_xml.py
@@ -1,7 +1,12 @@
- <?xml version="1.0" encoding="UTF-8"?>
-<note>
-  <to>Aaaa</to>
-  <from>BBBbb</from>
-  <heading>Reminder</heading>
-  <body>Don't forget me this weekend!</body>
-</note> 
+# generate_xml.py
+
+def generar_xml():
+    with open("weather.xml", "w", encoding="utf-8") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write('<weather>\n')
+        f.write('  <location>Madrid</location>\n')
+        f.write('  <temperature>22</temperature>\n')
+        f.write('</weather>\n')
+
+if __name__ == "__main__":
+    generar_xml()
